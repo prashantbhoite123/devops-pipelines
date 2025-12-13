fafb0308399a814ebf15f51048c3e86c9cef5dce
Prashant-penteai <prashant@penteai.com>
2025-12-13 17:04:47 +0530

    fix some issue

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("hello devops")
 console.log("Iam learning CI/CD using github actions");
+
+
+console.log("hey its me vaishanvi iam learning github");
+console.log("working on mnc company");
+
+
