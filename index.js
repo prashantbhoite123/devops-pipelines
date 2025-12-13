c20d61a12125801f8ceeadb45edba701b90c630e
Prashant-penteai <prashant@penteai.com>
2025-12-13 16:51:52 +0530

    working on basic feature

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("hello devops")
 console.log("Iam learning CI/CD using github actions");
+
+
+console.log("prashant is working on his branch")
+console.log("prashant is a chagpt developer");
+console.log("prashant is a bad boy");
